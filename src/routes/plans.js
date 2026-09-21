44fdc8458f6d1a706c53353e729f8c2ab2a60238
chinedu-freedom <chinedufreedom10@gmail.com>
2026-09-21 06:29:36 +0100

    Allow all users to invest in Activity Series packages

--- src/routes/plans.js
+++ src/routes/plans.js
@@ -61,26 +61,6 @@ router.post('/invest', authenticate, async (req, res) => {
           error: 'Activity Series packages are currently not active. Please check back later!'
         });
       }
-
-      // Check if user has an existing investment in a VIP Series package
-      const vipInvestment = await prisma.investments.findFirst({
-        where: {
-          user_id: userId,
-          plan: {
-            OR: [
-              { category: 'VIP Series' },
-              { category: null }
-            ]
-          }
-        }
-      });
-
-      if (!vipInvestment) {
-        return res.status(400).json({
-          success: false,
-          error: 'Only members who have activated a VIP Series package can purchase Activity Series packages. Please activate a VIP package first!'
-        });
-      }
     }
 
     const investAmount = Number(amount);
